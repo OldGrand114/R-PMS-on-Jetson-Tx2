b47345671affe797bdb8b2fee0ee77f3d453a134
93ahnjy <43025974+93ahnjy@users.noreply.github.com>
2018-12-26 16:59:32 +0900

    Update evaluation_command.py

--- mAP/evaluation_command.py
+++ mAP/evaluation_command.py
@@ -4,7 +4,7 @@ os.chdir('/home/tkol/darknet/results')
 os.system('cp *.txt ../../mAP/valid_result')
 print('valid_result redirec complete\n')
 os.chdir('/home/tkol/mAP/extra')
-os.system('python convert_gt_yolo.py')
+os.system('python convert_gt_yolo.py') # readme 맨 아래에 코드있는 거 다운받으면 된다.
 os.chdir('/home/tkol/mAP/mAP_predict 변환 코드/')
 os.system('./Valid_to_mAP')
 print('predicted conversion complete\n')
