771d62990546ed4f8d6ea9c5844dddb8763faf61
93ahnjy <43025974+93ahnjy@users.noreply.github.com>
2018-12-26 17:01:15 +0900

    Update evaluation_command.py

--- mAP/evaluation_command.py
+++ mAP/evaluation_command.py
@@ -13,6 +13,6 @@ print('predicted conversion complete\n')
 os.chdir('..')
 os.system('python3 car_truck_gt_converter.py')
 print('GT conversion complete\n')
-os.system('python3 car_truck_pre_converter.py')
+os.system('python3 car_truck_pre_converter.py') # car와 truck은 하나로 일단 병합 시켰다. truck 정확도 올라가면 따로 코드 변환 ㄱ 
 print('Predicted conversion complete\n')
 os.system('python3 main.py -na')
